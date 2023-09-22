0180f3cee0032102b9a5b6038ce9af8f33641640
Juxtaposed <juxtaposedwords@gmail.com>
2023-09-21 21:23:32 -0700

    adding in cp20 standards for intellisense

--- learncpp/007-compiling-first-program/HelloWorld.cpp
+++ learncpp/007-compiling-first-program/HelloWorld.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     std::cout << "Hello, whorl!";    
-    // Added to allow time for use to inspect (though vscode debug allows this already)
+    // Added to allow time for use to inspect (though vscode debug allows this)
     std::cin.clear(); //reset any error flags
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find a newline
     std::cin.get(); //get more char from teh user (Waits for you to press enter)
